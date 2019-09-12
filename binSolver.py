ae7d3faa43fc8acdc30e8d6deff5e81152445784
Rohil Tuli <rohil.tuli@gmail.com>
2019-09-12 12:37:42 -0400

    Functioning code, and gif of demo added

--- binSolver.py
+++ binSolver.py
@@ -13,6 +13,19 @@ scaleFactor = int(1200/size)
 def moveToPosition(i, j):
     pyautogui.moveTo(startLoc[1] + j*scaleFactor, startLoc[0] + i*scaleFactor)
 
+def similarRowOrColumn(x, y):
+    for i in range(size):
+        if x.data[i] + y.data[i] == 5:
+            return False
+    return True
+
+class RoC:
+    def __init__(self, id):
+        self.data = []
+        self.id = id
+        self.undetermined = size
+    def append(self, x):
+        self.data.append(x)
 
 class Board:
     def __init__(self, size):
@@ -20,6 +33,18 @@ class Board:
         self.numLeft = 0
         self.columns = []
         self.rows = []
+        self.compareColumns = []
+        self.compareRows = []
+    def similarRow(self, i1, i2):
+        for j in range(size):
+            if self.board[i1][j] + self.board[i2][j] == 5:
+                return False
+        return True
+    def similarColumn(self, j1, j2):
+        for i in range(size):
+            if self.board[i][j1] + self.board[i][j2] == 5:
+                return False
+        return True
     def input(self, img):
         for i in range(size):
             temp = []
@@ -35,21 +60,55 @@ class Board:
         for i in range(size):
             red = 0
             blue = 0
+            temp = RoC(i)
+            countBlanks = 0
             for x in self.board[i]:
+                temp.append(x)
+                if x == 1:
+                    countBlanks += 1
                 if x == 2:
                     blue += 1
                 elif x == 3:
                     red += 1
             self.rows.append([blue, red])
+            temp.undetermined = countBlanks
+            addedToExisting = False
+            for x in self.compareRows:
+                similar = True
+                for y in x:
+                    if not similarRowOrColumn(y, temp):
+                        similar = False
+                if similar:
+                    x.append(temp)
+                    addedToExisting = True
+            if not addedToExisting:
+                self.compareRows.append([temp])
         for j in range(size):
             red = 0
             blue = 0
+            temp = RoC(j)
+            countBlanks = 0
             for i in range(size):
+                temp.append(self.board[i][j])
+                if self.board[i][j] == 1:
+                    countBlanks += 1
                 if self.board[i][j] == 2:
                     blue += 1
                 elif self.board[i][j] == 3:
                     red += 1
             self.columns.append([blue, red])
+            temp.undetermined = countBlanks
+            addedToExisting = False
+            for x in self.compareColumns:
+                similar = True
+                for y in x:
+                    if not similarRowOrColumn(y, temp):
+                        similar = False
+                if similar:
+                    x.append(temp)
+                    addedToExisting = True
+            if not addedToExisting:
+                self.compareColumns.append([temp])
     def markBlue(self, i, j):
         self.board[i][j] = 2
         moveToPosition(i, j)
@@ -57,6 +116,54 @@ class Board:
         self.numLeft -= 1
         self.rows[i][0] += 1
         self.columns[j][0] += 1
+        rowData = []
+        rowUndetermined = 0
+        columnData = []
+        columnUndetermined = 0
+        for x in self.compareRows:
+            for y in x:
+                if y.id == i:
+                    y.data[j] = 2
+                    rowData = y.data
+                    rowUndetermined = y.undetermined - 1
+            x = [y for y in x if not y.id == i]
+        temp = RoC(i)
+        temp.undetermined = rowUndetermined
+        temp.data = rowData
+        addedToExisting = False
+        for x in self.compareRows:
+            similar = True
+            for y in x:
+                if not similarRowOrColumn(y, temp):
+                    similar = False
+            if similar:
+                x.append(temp)
+                addedToExisting = True
+        if not addedToExisting:
+            self.compareRows.append([temp])
+
+        for x in self.compareColumns:
+            for y in x:
+                if y.id == j:
+                    y.data[i] = 2
+                    columnData = y.data
+                    columnUndetermined = y.undetermined - 1
+            x = [y for y in x if not y.id == j]
+        temp2 = RoC(j)
+        temp2.undetermined = columnUndetermined
+        temp2.data = columnData
+        addedToExisting = False
+        for x in self.compareColumns:
+            similar = True
+            for y in x:
+                if not similarRowOrColumn(y, temp2):
+                    similar = False
+            if similar:
+                x.append(temp2)
+                addedToExisting = True
+        if not addedToExisting:
+            self.compareRows.append([temp2])
+
     def markRed(self, i, j):
         self.board[i][j] = 3
         moveToPosition(i, j)
@@ -64,6 +171,53 @@ class Board:
         self.numLeft -= 1
         self.rows[i][1] += 1
         self.columns[j][1] += 1
+        rowData = []
+        rowUndetermined = 0
+        columnData = []
+        columnUndetermined = 0
+        for x in self.compareRows:
+            for y in x:
+                if y.id == i:
+                    y.data[j] = 3
+                    rowData = y.data
+                    rowUndetermined = y.undetermined - 1
+            x = [y for y in x if not y.id == i]
+        temp = RoC(i)
+        temp.undetermined = rowUndetermined
+        temp.data = rowData
+        addedToExisting = False
+        for x in self.compareRows:
+            similar = True
+            for y in x:
+                if not similarRowOrColumn(y, temp):
+                    similar = False
+            if similar:
+                x.append(temp)
+                addedToExisting = True
+        if not addedToExisting:
+            self.compareRows.append([temp])
+
+        for x in self.compareColumns:
+            for y in x:
+                if y.id == j:
+                    y.data[i] = 3
+                    columnData = y.data
+                    columnUndetermined = y.undetermined - 1
+            x = [y for y in x if not y.id == j]
+        temp2 = RoC(j)
+        temp2.undetermined = columnUndetermined
+        temp2.data = columnData
+        addedToExisting = False
+        for x in self.compareColumns:
+            similar = True
+            for y in x:
+                if not similarRowOrColumn(y, temp2):
+                    similar = False
+            if similar:
+                x.append(temp2)
+                addedToExisting = True
+        if not addedToExisting:
+            self.compareRows.append([temp2])
     def checkForThrees(self):
         swapsMade = True
         while swapsMade:
@@ -147,14 +301,50 @@ class Board:
                 for i in range(size):
                     if self.board[i][x] == 1:
                         self.markBlue(i, x)
+    def checkForSimilarRowsOrColumns(self):
+        almostCompleteRows = []
+        completeRows = []
+        for x in range(len(self.rows)):
+            if self.rows[x][0] == size/2 and self.rows[x][1] == size/2:
+                completeRows.append(x)
+            elif self.rows[x][0] == (size/2 - 1) and self.rows[x][1] == (size/2 - 1):
+                almostCompleteRows.append(x)
+        for x in completeRows:
+            for y in almostCompleteRows:
+                if self.similarRow(x, y):
+                    for j in range(size):
+                        if self.board[y][j] == 1 and self.board[x][j] == 2:
+                            self.markRed(y, j)
+                        elif self.board[y][j] == 1 and self.board[x][j] == 3:
+                            self.markBlue(y, j)
+        almostCompleteColumns = []
+        completeColumns = []
+        for x in range(len(self.columns)):
+            if self.columns[x][0] == size/2 and self.columns[x][1] == size/2:
+                completeColumns.append(x)
+            elif self.columns[x][0] == (size/2 - 1) and self.columns[x][1] == (size/2 - 1):
+                almostCompleteColumns.append(x)
+        for x in completeColumns:
+            for y in almostCompleteColumns:
+                if self.similarColumn(x, y):
+                    for i in range(size):
+                        if self.board[i][y] == 1 and self.board[i][x] == 2:
+                            self.markRed(i, y)
+                        elif self.board[i][y] == 1 and self.board[i][x] == 3:
+                            self.markBlue(i, y)
+
     def solve(self):
-        # while numLeft != 0
-        while self.numLeft > 0:
+        while board.numLeft > 0:
+            initBoard = board.board
             self.checkForThrees()
             self.checkForDeterminedRowOrColumn()
+            if initBoard == board.board:
+                self.checkForSimilarRowsOrColumns()
+            #self.checkForSimilarRowsOrColumns()
+            # try checking for swaps by comparing board state, then if no swaps brute force check for similar rows/columns
 
-print("\nStarting in 2 seconds...")
-time.sleep(2)
+print("\nStarting in 3 seconds...")
+time.sleep(3)
 pyautogui.screenshot('myScreenshot.png')
 img = io.imread('myScreenshot.png')
 #print(pyautogui.position())
